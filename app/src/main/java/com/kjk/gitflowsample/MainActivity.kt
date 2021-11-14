0075a3438e35247204a8d114a4c82f1996106bff
JINKOO <jacob777300@gmail.com>
2021-11-14 19:40:59 +0900

    short 4 - bug fix

--- app/src/main/java/com/kjk/gitflowsample/MainActivity.kt
+++ app/src/main/java/com/kjk/gitflowsample/MainActivity.kt
@@ -3,6 +3,7 @@ package com.kjk.gitflowsample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.kjk.gitflowsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,13 +16,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-
         val name = binding.tvName.text.toString()
         Log.w("1111", name)
+
+        //TODO 이름을 토스트로 보여주는 버튼을 추가해야 한다.
+
     }
 
     // short branch에서 기능 개선(bug fix)
     private fun showToast(name: String) {
-
+        Toast.makeText(this, name, Toast.LENGTH_SHORT).show()
     }
 }
